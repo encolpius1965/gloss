bcb894252b6dbc53cf3d6b3b9027d0bc587205f3
encolpius1965 <44666906+encolpius1965@users.noreply.github.com>
2018-11-15 12:32:33 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,13 +2,18 @@
  session_start();
 header ("Content-Type: text/html; charset=utf-8");
 
-
+/* !!!
 if (!isset($_SESSION['UserId'])     OR ($_SESSION['UserId']==0)        )       
                 header('Location: verify.php');   
                 // $UserId = 1;
         else 
                $UserId=$_SESSION['UserId'];
 
+*/
+
+$UserId = 1;
+
+echo $UserId;
 
 // require_once( "classes/connection.php" );
 
