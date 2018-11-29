f85a4d6eca45a4cd1b43310b71a0c99ca7567b94
encolpius1965 <44666906+encolpius1965@users.noreply.github.com>
2018-11-29 22:59:42 +0300

    Add files via upload
    mysqli подключил

--- classes/connection.php
+++ classes/connection.php
@@ -26,7 +26,7 @@ function __construct()
 	$db_password = 'mysql';
 	$db_name = 'heroku_846065d530579e0';
 	$db_charset = 'utf8';
-
+/*
 // Реальная хероку
     $db_host = 'eu-cdbr-west-02.cleardb.net';
 	$db_username = 'b0f439327ec632';
@@ -34,20 +34,29 @@ function __construct()
 	$db_name = 'heroku_846065d530579e0';
 	$db_charset = 'utf8';
 
+  */  
     
-    
-    echo "<p> __Before  Connection";
+    echo "<p> 1__Before  Connection";
     
 	// $is_connected = @mysql_connect($db_host, $db_username, $db_password);
-    $is_connected = mysql_connect($db_host, $db_username, $db_password);
-    echo "<p> After MySQL Connection. is_connected=$is_connected";
+// !    $is_connected = mysql_connect($db_host, $db_username, $db_password);
+//    echo "<p> After MySQL Connection. is_connected=$is_connected";
     
-	$is_db_selected = $is_connected ? @mysql_select_db($db_name) : FALSE; 
+ $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name); // подключаемся к базе MySQL   
+
+   echo "<p> After DB Connection. ";
+
  
-   echo "<p> After DB Connection. is_db_selected=$is_db_selected";
+if (mysqli_connect_errno()) { // проверяем подключение
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+}    
+    
+// !	$is_db_selected = $is_connected ? @mysql_select_db($db_name) : FALSE; 
+ 
  
 	$errors = array();
- 
+ /*
 	if (!$is_connected) $errors[] = 'Не могу соединиться с базой данных';
 	if (!$is_db_selected) $errors[] = 'Не могу найти базу данных ';
  
@@ -61,7 +70,7 @@ function __construct()
 		}
 		echo '</ul>';
     }    
- 
+ */
 }
 
 function GetUserId()
