99f523deed251d87fb92ca26e39d6eb6666e9c3f
encolpius1965 <44666906+encolpius1965@users.noreply.github.com>
2019-05-18 22:58:18 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,245 +1,4 @@
-<?php// echo("<p> NOS");function GetUserId(){// проверяем наличие корректного UserId по массивам SESSION и COOKIES       $ret=0;        if (isset($_SESSION['UserId']))                  $ret=$_SESSION['UserId'];        if ($ret==0)    {        if  (isset($_COOKIE['UserId']))                  $ret=$_COOKIE['UserId'];    }        return $ret;    }
-
- session_start();
-// require_once( "util/util.php" );
-
-header('Content-Type: text/html; charset=utf-8');
-$UserId=1;
-
-// $UserId =  GetUserId();
-// echo "<p>UserId=$UserId";
-
-if     ($UserId==0)   
-                header('Location: verify.php');   
-                // $UserId = 1;
-
-                      
-require_once( "classes/connection.php" );
-
-
-
-
-/*
-if (!isset($_SESSION['UserId']))  
-                header('Location: concept.php');   
-                // $UserId = 1;
-        else 
-               $UserId=$_SESSION['UserId'];
-
-*/
-$conn = new Connection();
-
-   
-
-
-
-
-$TypeSingleUsMess=0;
-$TypeSelectUsMess=1;
- 
-$row = $conn->GetRow("USER", "USER_ID=$UserId"); 	
- 
- 
-			      $ScoreMax= $row[SCOREMAX];
-                
-                  $ScoreStep=$row[SCORESTEP];
-                  $MonthlyShrinkage=$row[MSHRINKAGE];
-                  $lSelect=$row[LSELECT];
-                  $lStarted=$row[LSTARTED];
-                  $ConceptSourceId=$row[CONC_SOURCE_ID];
-                  $ConceptTargetId=$row[CONC_TARGET_ID];
-                  $SessionId = $row[SESSION_ID];
-                  $SumScore = $row[SUMSCORE];
-                  $Count = $row[COUNT];
-                  $Average = $row[AVERAGE];
-                  $SumFail = $row[SUMFAIL];
-                  $Kpd = $row[KPD];
-                  $lResult=$row[LRESULT];
-                  $lexTarget=$row[LEX_TARGET];
-                  $lexSource=$row[LEX_SOURCE];
-                  $SelectSize=$row[SEL_SIZE];
-
-      
-        
- // обработчик кнопки если кнопка была нажата, данные надо записатт на сервер из форм
- // 
- if  (!empty($_POST['f_start']))  // отработка 
- {
-   //     $lStarted=_SwitchStarted($lHeaderInvocation=false);
-     
-         $ConceptSourceId=empty($_POST['SelectConceptSource']) ?  $ConceptSourceId : $_POST['SelectConceptSource'];
-         $ConceptTargetId=empty($_POST['SelectConceptTarget']) ?  $ConceptTargetId : $_POST['SelectConceptTarget'];
-         $ScoreMax= empty($_POST['ScoreMax']) ? $ScoreMax : $_POST['ScoreMax'];
-         $ScoreStep= empty($_POST['ScoreStep']) ? $ScoreStep : $_POST['ScoreStep'];
-         $MonthlyShrinkage= empty($_POST['MonthlyShrinkage']) ? $MonthlyShrinkage : $_POST['MonthlyShrinkage'];
-         $lSelect= ($_POST['FillingStyle'])=="Select" ? 1: 0;     
-         $SelectSize=empty($_POST['SelectSize']) ?  $SelectSize : $_POST['SelectSize'];         
- //                         echo "<p>перед переключением lStarted=$lStarted";
-         $lStarted = ($lStarted==1) ? 0 : 1;
-          
-         
- //                         echo "<p>после переключения lStarted=$lStarted";
-         if ($lStarted)
-         {
-                $SessionId++;
-                $SumScore=0;
-                $SumFail=0;
-                $lResult=0;
-                $Kpd=0;
-         }      
-         
-         
-        $conn->UpdateTable("USER",
-                            Array(    
-                 "SCOREMAX"=>$ScoreMax, "SCORESTEP"=>$ScoreStep, "MSHRINKAGE"=>$MonthlyShrinkage, "LSELECT"=>$lSelect , "LSTARTED"=>$lStarted,                                   "CONC_SOURCE_ID"=>$ConceptSourceId,   "CONC_TARGET_ID"=>$ConceptTargetId, "SESSION_ID"=>$SessionId,                 "SUMSCORE"=>$SumScore, "SUMFAIL"=>$SumFail,   "KPD"=>$Kpd,   "LRESULT"=>$lResult, "SEL_SIZE"=>$SelectSize                 ), 
-                 "USER_ID=$UserId");				
-
-                 /*
-                 ,"CONC_SOURCE_ID"=>$ConceptSourceId,   "CONC_TARGET_ID"=>$ConceptTargetId, "SESSION_ID"=>$SessionId, "SESSION_TIME"=>NOW(),
-                 "SUMSCORE"=>$SumScore, "SUMFAIL"=>$SumFail,   "KPD"=>$Kpd,   "LRESULT"=>$lResult, "SEL_SIZE"=$SelectSize
-                 */
-        
-
-  }      
-  
-  
-
-   if   ( ((!empty($_POST['f_start']))&&  $lStarted    )  ||  (    !empty($_POST['f_affirm'])       ) ) 
-  {
-      
-            //этот кусок мы потом перенесем в фунцию. это вывод Source-параметров   
-                 $conn->Proc("GetSourceRec", Array($UserId));
-                                
-                                //??????????????? разбираться и видимо убрать то что выше
-                 $row=$conn->GetRow("USER", "USER_ID=$UserId");                                
-                                                $lexSource= $row[LEX_SOURCE];
-                                                $Count = $row[COUNT];
-                                                $Average = $row[AVERAGE];
-
-                        
-  // а если у нас стиль - выбор из, то нам надо и с массивом возможного выбора разбираться
-                if ($lSelect == 1)     
-                {
-                    $conn->Proc("GetSelectionArray", Array($UserId));
-                } 
-  // очистим LexTarget и lResult и запишем lexSource
-                $lexTarget = "";    // '".$name."    '".$name."    '".$name."'
-                $lResult =0;  //  '".$lexTarget."'
-                
-                $conn->UpdateTable("USER",
-                                    Array("LEX_TARGET"=>$lexTarget,  "LRESULT"=>$lResult),
-                                           "USER_ID=$UserId");
-		
-                        
-  }   
-  
- 
-  if ($lStarted && ($lSelect == 1))
-  {
-                                $aSelTarget=$conn->GetColumn("USMESS","LEX_ID", "TXT", "USER_ID=$UserId AND LTYPE=$TypeSelectUsMess");                                              
-                                $countSelA = count($aSelTarget);   //                             print_r($aSelTarget);
-
-  }
-
- 
-  
-  if  (!empty($_POST['f_put']))  // отработка 
-  {
-      if ($lSelect == 0)
-      {
-            $lexTarget = $_POST['lexTarget'];    // '".$name."    '".$name."    '".$name."'
-  
- 
-      }     
-      else
-      {   
-            $lexTarget=  empty($_POST['aSelTarget']) ? "" :     $_POST['aSelTarget'];
-             /*
-             echo $lexTargetId;
-            $sql = "SELECT * FROM LEX WHERE LEX_ID=$lexTargetId";
-            $result = mysql_query($sql) 
-                                or die('Query error: <code>'.$sql.'</code>');
-                                if ( is_resource($result) ) 
-                                    {
-			
-                                        while ( $row = mysql_fetch_assoc($result) )
-                                        {
-                                                $lexTarget=  $row[TXT];
-
-                                        }    
-                                    }   
-
-            */
-            
-            
-      }
- 
-               $conn->Proc("PutTargetRec", Array($UserId, $lexTarget));
-  
-
-  } 
-
-    
-    if ($lStarted)            // здесь аккуратно считывается информация из трех баз и выводится в условно-мадальное окно
-    {
-               // конечно это окно методами тупого html не очень красиво. надо почитаь матчасть.
-                    $row = $conn->GetRow("USER", "USER_ID=$UserId"); 	
-                           $SumScore = $row[SUMSCORE];
-                           $Kpd = $row[KPD];
-                           $lResult=$row[LRESULT];
-                           $Score= $row[SCORE];
-                  // $SessionTime = $row[SESSION_TIME];   неочевидно. зачем нам на фронтенде эта переменная.
-                         
-                        $aResult[0] =  "";         
-                        $aResult[1] = "Ошибка";
-                        $aResult[2] = "Верно";
-                        $aResult[3] = "<b>Верно</b>. Лексема выведена из активного набора";
-                        
-                        $aResultClass[0]="thinking";
-                        $aResultClass[1]="wrong";
-                        $aResultClass[2]="right";
-                        $aResultClass[3]="good";
-                        
-                        $curClassResult  = $aResultClass[$lResult]; 
-         // заполним справочник концептов
-         
-                    $RightAnswerTitle = (lResult==1) ? "Правильный ответ" : "Дополнительные варианты";      
-                    
-//                    echo "<p>aRightAnswer";
-//                    echo "<p> TypeSingleUsMess=$TypeSingletUsMess";
-                    $aRightAnswer=$conn->GetOnlyColumn("USMESS", "TXT", "USER_ID=$UserId AND LTYPE=$TypeSingleUsMess AND ( ($lResult=1) OR (LAUXILIARY=1)           )");                                              
-                    $countRA = count($aRightAnswer);
-                    $lIsAnswerArray = (($lResult>0) AND ($countRA>0));
-                    $RightAnswerTitle =
-                               ($lIsAnswerArray) ?
-                                                (($lResult==1) ? "Правильный ответ" : "Дополнительные варианты")
-                                                :
-                                                "";
-                    
-  }  
-  
-  //   print_r($aConceptSource);
- /*      <?=($key==$ConceptTargetId)? " selected": ""?>*/
- 
-       $lShowLexems = (($lStarted ==1) and ($Count >0)) ? 1 : 0;
-       $lCountDisclaimer = (($lStarted ==1) and ($Count ==0 )) ? 1 : 0;
-       
-       $lShowTargetString = ($lSelect==0) || (($lSelect==1) && ($lResult>0)) ? 1: 0;
-       $lShowTargetArrray =(($lSelect==1) && ($lResult==0))  ? 1: 0;
-       
-       
-       $StartStopStr = $lStarted ? "Остановить тестирование" : "Начать тестирование";
-        $StartStopStr = $lCountDisclaimer ?"       OK       "  : $StartStopStr;
-       
-       
-       $CheckedSelect= $lSelect ? "checked":"";
-       $CheckedFilling= $lSelect ? "": "checked";
-
-  // заполним справочник концептов
-   $aConceptSource=$conn->GetColumn("CONCEPT","CONCEPT_ID", "NAME", "USER_ID=$UserId");                                              
-   $aConceptTarget=$conn->GetColumn("CONCEPT","CONCEPT_ID", "NAME", "USER_ID=$UserId");       // в PHP нет нормальной функции клонирования массива. свою писать лень.
-   $sql = "SELECT * FROM CONCEPT WHERE USER_ID=$UserId"; 
-		
+<?php// echo("<p> NOS");		
 ?>
 <html>
 <head>
